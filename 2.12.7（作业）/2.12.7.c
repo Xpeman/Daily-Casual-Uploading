8939869a7bbeb571e3a63d789b1d49f2a990827f
Xpeman <2380385007@qq.com>
2024-08-08 20:17:42 +0800

    测试

---
+++ 2.12.7（作业）/2.12.7.c
@@ -0,0 +1,19 @@
+//打印Smile
+#include<stdio.h>
+int main(void)
+{
+	xiao();//调用函数
+	xiao();
+	xiao();
+	printf("\n");//换行
+	xiao();
+	xiao();
+	printf("\n");
+	xiao();
+	return 0;
+}
+int xiao(void)
+{
+	printf("Smile!");
+	return 0;
+}
